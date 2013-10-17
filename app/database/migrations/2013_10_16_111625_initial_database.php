77db917817142ccbbc7b43a9a22ad09d52bd1444
AsicsKoki <cpt.koki@gmail.com>
2013-10-17 13:25:07 +0200

    created migrations

--- app/database/migrations/2013_10_16_111625_initial_database.php
+++ app/database/migrations/2013_10_16_111625_initial_database.php
@@ -30,13 +30,14 @@ class InitialDatabase extends Migration {
 			$table->create();
 			$table->engine = 'InnoDB';
 			$table->increments('id');
-			$table->string('user_id');
+			$table->integer('user_id')->unsigned();
 			$table->string('name');
-		    $table->integer('color_id');
+		    $table->integer('color_id')->unsigned();
 		    $table->integer('price');
 		    $table->integer('quantity');
 			$table->string('description', 300)->nullable();
 			$table->integer('active');
+			$table->index('user_id');
 		});
 
 		Schema::table('comments', function($table)
@@ -44,45 +45,118 @@ class InitialDatabase extends Migration {
 			$table->create();
 			$table->engine = 'InnoDB';
 			$table->increments('id');
-			$table->string('user_id');
-			$table->string('product_id');
-			$table->string('text', 300)->nullable();
+			$table->integer('user_id')->unsigned();
+			$table->integer('product_id')->unsigned();
+			$table->string('comment', 300)->nullable();
 			$table->integer('approved');
 			$table->timestamp('posted_at');
+			$table->index('user_id');
+			$table->index('product_id');
 		});
 
 		Schema::table('colors', function($table)
 		{
 			$table->create();
 			$table->engine = 'InnoDB';
-			$table->increments('id');
-			$table->string('id');
-			$table->string('color');
+			$table->integer('id');
+			$table->string('color_name');
 		});
 
 		Schema::table('categorized_products', function($table)
 		{
 			$table->create();
 			$table->engine = 'InnoDB';
-			$table->integer('category_id');
-			$table->integer('product_id');
+			$table->integer('category_id')->unsigned();
+			$table->integer('product_id')->unsigned();
 		});
 
-		Schema::table('categorized_products', function($table)
+		Schema::table('comment_likes', function($table)
 		{
 			$table->create();
 			$table->engine = 'InnoDB';
-			$table->integer('category_id');
-			$table->integer('product_id');
-		});
-
-		Schema::table('likes', function($table)
-		{
-			$table->create();
-			$table->engine = 'InnoDB';
-			$table->integer('comment_id');
-			$table->integer('user_id');
+			$table->integer('comment_id')->unsigned();
+			$table->integer('user_id')->unsigned();
 			$table->increments('id');
+			$table->index('commenT_id');
+			$table->index('user_id');
+		});
+
+		Schema::table('roles', function($table)
+		{
+			$table->create();
+			$table->engine = 'InnoDB';
+			$table->integer('id');
+			$table->string('role', 300);
+		});
+
+		Schema::table('categories', function($table)
+		{
+			$table->create();
+			$table->engine = 'InnoDB';
+			$table->increments('id');
+			$table->integer('parent_id')->nullable()->unsigned();
+			$table->string('name', 300);
+			$table->index('parent_id');
+		});
+
+		Schema::table('banners', function($table)
+		{
+			$table->create();
+			$table->engine = 'InnoDB';
+			$table->increments('id');
+			$table->string('banner', 300);
+		});
+
+		Schema::table('product_ratings', function($table)
+		{
+			$table->create();
+			$table->engine = 'InnoDB';
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->integer('product_id')->unsigned();
+			$table->integer('rating');
+			$table->timestamp('rated_at');
+			$table->index('user_id');
+			$table->index('product_id');
+		});
+
+		Schema::table('images', function($table)
+		{
+			$table->create();
+			$table->engine = 'InnoDB';
+			$table->increments('id');
+			$table->integer('entity_id');
+			$table->string('entity_type', 300);
+			$table->string('entity_name', 300);
+			$table->string('image_name', 300);
+		});
+
+		Schema::table('products', function($table)
+		{
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+		});
+
+		Schema::table('comments', function($table)
+		{
+			$table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+		});
+
+		Schema::table('comment_likes', function($table)
+		{
+			$table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+		});
+
+		Schema::table('categories', function($table)
+		{
+			$table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
+		});
+
+		Schema::table('product_ratings', function($table)
+		{
+			$table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 		});
 	}
 
@@ -93,6 +167,16 @@ class InitialDatabase extends Migration {
 	 */
 	public function down()
 	{
+		Schema::drop('categorized_products');
+		Schema::drop('comment_likes');
+		Schema::drop('product_ratings');
+		Schema::drop('roles');
+		Schema::drop('categories');
+		Schema::drop('colors');
+		Schema::drop('images');
+		Schema::drop('banners');
+		Schema::drop('comments');
+		Schema::drop('products');
 		Schema::drop('users');
 	}
 
