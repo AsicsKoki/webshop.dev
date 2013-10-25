93e35ce293668edd9b62fe51c31873bc555d5fad
AsicsKoki <cpt.koki@gmail.com>
2013-10-25 09:24:02 +0200

    minor edit

--- app/routes.php
+++ app/routes.php
@@ -33,3 +33,4 @@ Route::put('users', 'UsersController@createUser');
 Route::get('products/{pid}', array('as' => 'ShowProductPage', 'uses' => 'ProductController@getProduct'))->where('pid', '\d+');
 Route::get('products', array('as' => 'AllProducts', 'uses' => 'ProductController@getProducts'));
 Route::post('products/{pid}', array('as' => 'UpdateProduct', 'uses' => 'ProductController@postProduct'))->where('pid', '\d+');
+Route::get('products/{pid}', array('as' => 'ShowProductEditPage', 'uses' => 'ProductController@getProduct'))->where('pid', '\d+');
