fbc75e26890b05b1e1d386cc590f2cac2a07fdc5
AsicsKoki <cpt.koki@gmail.com>
2014-01-13 00:52:06 +0100

    reworked new product validation

--- app/views/product/newProduct.blade.php
+++ app/views/product/newProduct.blade.php
@@ -2,12 +2,17 @@
 @section('main')
 	<div class="span6">
 		<div>
-			{{ Former::open()->class('form-horizontal pull-left')->method('put')->enctype('multipart/form-data')->action(URL::route('putNewProduct'))}}
+			{{ Former::open()->rules(array(
+				'name'     => 'required|max:50',
+				'description' => 'required|max:500',
+				'price'    => 'number',
+				'quantity' => 'number|int',
+				'avatar'   => 'image',))->class('form-horizontal pull-left')->method('put')->enctype('multipart/form-data')->action(URL::route('putNewProduct'))}}
 			<div class='control-group'>
-				{{ Former::text('name')->required()->label('name')->maxlength(50) }}
+				{{ Former::text('name')->required()->label('name') }}
 			</div>
 			<div class='control-group'>
-				{{ Former::textarea('description')->required()->label('description')->maxlength(500) }}
+				{{ Former::textarea('description')->required()->label('description') }}
 			</div>
 			<div class='control-group'>
 				{{ Former::number('price')->required()->label('price') }}
