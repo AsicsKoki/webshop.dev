c2ff9f628dbcdaaca17fff7f370079a5ab973498
AsicsKoki <cpt.koki@gmail.com>
2014-03-04 15:31:04 +0100

    debuging

--- app/controllers/PaypalPaymentController.php
+++ app/controllers/PaypalPaymentController.php
@@ -18,7 +18,7 @@ class PaypalPaymentController extends BaseController {
     private $_ClientSecret='EFQgdxAiBDDwkg_J83KxGEhC1_q7Ri6N9W4PoXlXHKtRhECCPElPxCZp4VJ-';
 
     /*
-	*   These construct set the SDK configuration dynamiclly, 
+	*   These construct set the SDK configuration dynamiclly,
 	*   If you want to pick your configuration from the sdk_config.ini file
 	*   make sure to update you configuration there then grape the credentials using this code :
 	*   $this->_cred= Paypalpayment::OAuthTokenCredential();
@@ -26,10 +26,10 @@ class PaypalPaymentController extends BaseController {
     public function __construct()
     {
 	   // ### Api Context
-	   // Pass in a `ApiContext` object to authenticate 
-	   // the call. You can also send a unique request id 
+	   // Pass in a `ApiContext` object to authenticate
+	   // the call. You can also send a unique request id
 	   // (that ensures idempotency). The SDK generates
-	   // a request id if you do not pass one explicitly. 
+	   // a request id if you do not pass one explicitly.
 
 	   $this->_apiContext = Paypalpayment:: ApiContext(
 		  Paypalpayment::OAuthTokenCredential(
@@ -62,86 +62,20 @@ class PaypalPaymentController extends BaseController {
 		// $firstName        = Input::get('firstName');
 		// $lastName         = Input::get('lastName');
 
-		$data = Input::all();
-    		Queue::push('PaymentQueue', $data)
-
-
-		// ### CreditCard
-		// A resource representing a credit card that can be
-		// used to fund a payment.
-		$card = Paypalpayment::CreditCard();
-		$card->setType($type);
-		$card->setNumber($creditCardNumber);
-		$card->setExpire_month($expireMonth);
-		$card->setExpire_year($expireYear);
-		$card->setCvv2($cvv2);
-		$card->setFirst_name($firstName);
-		$card->setLast_name($lastName);
-
-		// ### FundingInstrument
-		// A resource representing a Payer's funding instrument.
-		// Use a Payer ID (A unique identifier of the payer generated
-		// and provided by the facilitator. This is required when
-		// creating or using a tokenized funding instrument)
-		// and the `CreditCardDetails`
-		$fi = Paypalpayment::FundingInstrument();
-		$fi->setCredit_card($card);
-
-		// ### Payer
-		// A resource representing a Payer that funds a payment
-		// Use the List of `FundingInstrument` and the Payment Method
-		// as 'credit_card'
-		$payer = Paypalpayment::Payer();
-		$payer->setPayment_method("credit_card");
-		$payer->setFunding_instruments(array($fi));
-
-		// ### Amount
-		// Let's you specify a payment amount.
-		$amount = Paypalpayment:: Amount();
-		$amount->setCurrency("USD");
-		$amount->setTotal($total);
-
-		// ### Transaction
-		// A transaction defines the contract of a
-		// payment - what is the payment for and who
-		// is fulfilling it. Transaction is created with
-		// a `Payee` and `Amount` types
-		$transaction = Paypalpayment:: Transaction();
-		$transaction->setAmount($amount);
-		$transaction->setDescription("This is the payment description.");
-
-		// ### Payment
-		// A Payment Resource; create one using
-		// the above types and intent as 'sale'
-		$payment = Paypalpayment:: Payment();
-		$payment->setIntent("sale");
-		$payment->setPayer($payer);
-		$payment->setTransactions(array($transaction));
-
-		// ### Create Payment
-		// Create a payment by posting to the APIService
-		// using a valid ApiContext
-		// The return object contains the status;
-		try {
-			$payment->create($this->_apiContext);
-		} catch (\PPConnectionException $ex) {
-			return "Exception: " . $ex->getMessage() . PHP_EOL;
-			var_dump($ex->getData());
-			exit(1);
-		}
-
-		$response=$payment->toArray();
-
-		echo"<pre>";
-		print_r($response);
-
-		//var_dump($payment->getId());
-
-		//print_r($payment->toArray());//$payment->toJson();
-		}
-
+		$data = array(
+				'type'             => Input::get('creditCardType'),
+				'creditCardNumber' => Input::get('creditCardNumber'),
+				'cvv2'             => Input::get('creditCardSecurityNumber'),
+				'expireMonth'      => Input::get('creditCardExpiryMonth'),
+				'expireYear'       => Input::get('creditCardExpiryYear'),
+				'firstName'        => Input::get('firstName'),
+				'lastName'         => Input::get('lastName'),
+				'total'            => $total
+    			);
+    		Queue::push('PaymentQueue', $data);
+	}
     /*
-	   Use this call to get a list of payments. 
+	   Use this call to get a list of payments.
 	   url:payment/
     */
     public function index()
--- app/queues/PaymentQueue.php
+++ app/queues/PaymentQueue.php
@@ -1,81 +1,88 @@
 <?php
 class PaymentQueue {
 
-    public function fire($job, $data)
-    {
-		// ### CreditCard
-		// A resource representing a credit card that can be
-		// used to fund a payment.
-		$card = Paypalpayment::CreditCard();
-		$card->setType($type);
-		$card->setNumber($creditCardNumber);
-		$card->setExpire_month($expireMonth);
-		$card->setExpire_year($expireYear);
-		$card->setCvv2($cvv2);
-		$card->setFirst_name($firstName);
-		$card->setLast_name($lastName);
 
-		// ### FundingInstrument
-		// A resource representing a Payer's funding instrument.
-		// Use a Payer ID (A unique identifier of the payer generated
-		// and provided by the facilitator. This is required when
-		// creating or using a tokenized funding instrument)
-		// and the `CreditCardDetails`
-		$fi = Paypalpayment::FundingInstrument();
-		$fi->setCredit_card($card);
+	/**
+	* object to authenticate the call.
+	* @param object $_apiContext
+	*/
+	private $_apiContext;
 
-		// ### Payer
-		// A resource representing a Payer that funds a payment
-		// Use the List of `FundingInstrument` and the Payment Method
-		// as 'credit_card'
-		$payer = Paypalpayment::Payer();
-		$payer->setPayment_method("credit_card");
-		$payer->setFunding_instruments(array($fi));
+	public function fire($job, $data)
+	{
 
-		// ### Amount
-		// Let's you specify a payment amount.
-		$amount = Paypalpayment:: Amount();
-		$amount->setCurrency("USD");
-		$amount->setTotal($total);
 
-		// ### Transaction
-		// A transaction defines the contract of a
-		// payment - what is the payment for and who
-		// is fulfilling it. Transaction is created with
-		// a `Payee` and `Amount` types
-		$transaction = Paypalpayment:: Transaction();
-		$transaction->setAmount($amount);
-		$transaction->setDescription("This is the payment description.");
+	// ### CreditCard
+	// A resource representing a credit card that can be
+	// used to fund a payment.
+	$card = Paypalpayment::CreditCard();
+	$card->setType($data["type"]);
+	$card->setNumber($data["creditCardNumber"]);
+	$card->setExpire_month($data["expireMonth"]);
+	$card->setExpire_year($data["expireYear"]);
+	$card->setCvv2($data["cvv2"]);
+	$card->setFirst_name($data["firstName"]);
+	$card->setLast_name($data["lastName"]);
 
-		// ### Payment
-		// A Payment Resource; create one using
-		// the above types and intent as 'sale'
-		$payment = Paypalpayment:: Payment();
-		$payment->setIntent("sale");
-		$payment->setPayer($payer);
-		$payment->setTransactions(array($transaction));
+	// ### FundingInstrument
+	// A resource representing a Payer's funding instrument.
+	// Use a Payer ID (A unique identifier of the payer generated
+	// and provided by the facilitator. This is required when
+	// creating or using a tokenized funding instrument)
+	// and the `CreditCardDetails`
+	$fi = Paypalpayment::FundingInstrument();
+	$fi->setCredit_card($card);
 
-		// ### Create Payment
-		// Create a payment by posting to the APIService
-		// using a valid ApiContext
-		// The return object contains the status;
-		try {
-			$payment->create($this->_apiContext);
-		} catch (\PPConnectionException $ex) {
-			return "Exception: " . $ex->getMessage() . PHP_EOL;
-			var_dump($ex->getData());
-			exit(1);
-		}
+	// ### Payer
+	// A resource representing a Payer that funds a payment
+	// Use the List of `FundingInstrument` and the Payment Method
+	// as 'credit_card'
+	$payer = Paypalpayment::Payer();
+	$payer->setPayment_method("credit_card");
+	$payer->setFunding_instruments(array($fi));
 
-		$response=$payment->toArray();
+	// ### Amount
+	// Let's you specify a payment amount.
+	$amount = Paypalpayment:: Amount();
+	$amount->setCurrency("USD");
+	$amount->setTotal($data["total"]);
 
-		echo"<pre>";
-		print_r($response);
+	// ### Transaction
+	// A transaction defines the contract of a
+	// payment - what is the payment for and who
+	// is fulfilling it. Transaction is created with
+	// a `Payee` and `Amount` types
+	$transaction = Paypalpayment:: Transaction();
+	$transaction->setAmount($amount);
+	$transaction->setDescription("This is the payment description.");
 
-		//var_dump($payment->getId());
+	// ### Payment
+	// A Payment Resource; create one using
+	// the above types and intent as 'sale'
+	$payment = Paypalpayment:: Payment();
+	$payment->setIntent("sale");
+	$payment->setPayer($payer);
+	$payment->setTransactions(array($transaction));
 
-		//print_r($payment->toArray());//$payment->toJson();
-		}
-    }
+	// ### Create Payment
+	// Create a payment by posting to the APIService
+	// using a valid ApiContext
+	// The return object contains the status;
+	try {
+		$payment->create($this->_apiContext);
+	} catch (\PPConnectionException $ex) {
+		return "Exception: " . $ex->getMessage() . PHP_EOL;
+		var_dump($ex->getData());
+		exit(1);
+	}
 
+	$response=$payment->toArray();
+
+	echo"<pre>";
+	print_r($response);
+
+	//var_dump($payment->getId());
+
+	//print_r($payment->toArray());//$payment->toJson();
+	}
 }
